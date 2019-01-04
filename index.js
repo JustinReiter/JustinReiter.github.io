6100bd711077808669a6413f7e1c9a9a986641a8
Justin Reiter <JDReiter@uwaterloo.ca>
2019-01-03 20:27:03 -0500

    Rework javascript into separate file

--- index.js
+++ index.js
@@ -2,14 +2,24 @@
 
 
 function typeName() {
-    var speed = 50;
-    var name = "Justin Reiter";
+    let speed = 50;
+    let name = "Justin Reiter";
 
-    if (document.getElementById("name").innerHTML.length() < name.length()) {
+    if (document.getElementById("name").innerHTML.length < name.length) {
         document.getElementById("name").innerHTML += name.charAt(document.getElementById("name").length());
         setTimeout(typeName, speed);
     }
 }
 
+function navBar() {
+    var x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
-window.onload = typeName();
+window.onload = function() {
+    typeName();
+};
