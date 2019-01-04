46632be341d8905d03c1b15aa05e0ee1b778e5c4
Justin Reiter <JDReiter@uwaterloo.ca>
2019-01-03 21:02:43 -0500

    Fix typeName

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 
 
-
+var i = 0;
 function typeName() {
     let speed = 50;
-    let name = "Justin Reiter";
+    let name = "I am Justin Reiter";
+    if (document.getElementById("name").innerHTML === "Hello!") {
 
-    if (document.getElementById("name").innerHTML.length < name.length) {
-        document.getElementById("name").innerHTML += name.charAt(document.getElementById("name").length());
+        document.getElementById("name").innerHTML = "";
+    }
+    if (i < name.length) {
+        document.getElementById("name").innerHTML += name.charAt(i);
+        i++
         setTimeout(typeName, speed);
     }
 }
+window.onload = typeName;
 
 function navBar() {
     var x = document.getElementById("topnav");
@@ -20,6 +25,4 @@ function navBar() {
     }
 }
 
-window.onload = function() {
-    typeName();
-};
+
