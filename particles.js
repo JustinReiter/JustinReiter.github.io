31c75b2ba609e14f66283c36e57cfffd6d8e796b
Justin Reiter <JDReiter@uwaterloo.ca>
2019-01-17 00:06:21 -0500

    Implement form and footer

--- particles.js
+++ particles.js
@@ -510,14 +510,6 @@ var pJS = function(tag_id, params){
       /* the particle */
       var p = pJS.particles.array[i];
 
-      // var d = ( dx = pJS.interactivity.mouse.click_pos_x - p.x ) * dx + ( dy = pJS.interactivity.mouse.click_pos_y - p.y ) * dy;
-      // var f = -BANG_SIZE / d;
-      // if ( d < BANG_SIZE ) {
-      //     var t = Math.atan2( dy, dx );
-      //     p.vx = f * Math.cos(t);
-      //     p.vy = f * Math.sin(t);
-      // }
-
       /* move the particle */
       if(pJS.particles.move.enable){
         var ms = pJS.particles.move.speed/2;
