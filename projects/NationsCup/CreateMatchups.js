51e360acb61c924d0f5ae3e8c8a3dede3c8e7f03
Justin Reiter <JDReiter@uwaterloo.ca>
2019-10-05 23:20:25 -0400

    Fix error with player matchup creation

--- projects/NationsCup/CreateMatchups.js
+++ projects/NationsCup/CreateMatchups.js
@@ -30,14 +30,20 @@ function matchTeamMembers(team1, team2) {
     }
 
     while (extendedTeam1.length < 12) {
-        const val = Math.ceil(Math.random() * team1.length);
+        let val = Math.floor(Math.random() * team1.length);
+        if (val >= team1.length) {
+            continue;
+        }
         if (!matchedPlayers.has(team1[val].playerId)) {
             matchedPlayers.set(team1[val].playerId, true);
             extendedTeam1.push(team1[val]);
         }
     }
     while (extendedTeam2.length < 12) {
-        const val = Math.ceil(Math.random() * team2.length);
+        let val = Math.floor(Math.random() * team2.length);
+        if (val >= team2.length) {
+            continue;
+        }
         if (!matchedPlayers.has(team2[val].playerId)) {
             matchedPlayers.set(team2[val].playerId, true);
             extendedTeam2.push(team2[val]);
